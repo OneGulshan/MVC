1c857f1eeb5a7f69b2e3842ebe613769df50a995
OneGulshan <gulshankumar.mailid01@gmail.com>
2023-07-12 00:06:41 +0530

    DataAnnotationCompleated

--- MVC/Models/Employee.cs
+++ MVC/Models/Employee.cs
@@ -11,18 +11,27 @@ namespace MVC.Models
         [Required(ErrorMessage = "Name is mandatory")]//Custom Message
         [StringLength(20, MinimumLength = 5, ErrorMessage = "Name Should be in between 5 and 20")]
         public string EmployeeName { get; set; }
+        [Required]
         [Range(20,60, ErrorMessage = "Age should be in the range of 20 and 60")]
         public int? EmployeeAge { get; set; }
-        public string EmployeeGender { get; set; }
+        [Required]
         [RegularExpression("^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$", ErrorMessage = "Invalid Email")]
         public string EmployeeEmail { get; set; }
+        [Required]
         [RegularExpression(@"(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$", ErrorMessage = "UpperCase, LowerCase, Numbers, Symbols, 8 Characters")]//@ <- Verbatim Litteral For Esscape Sequences reading as a normal character like \d etc.
+        [DataType(DataType.Password)]//DataType Only Works With EditorFor
+        //[DataType(DataType.Date)]//For Calendar Showing For Date Choosing
         public string EmpPassword { get; set; }
+        [Required]
         [Compare("EmpPassword",ErrorMessage = "Password is not identical")]//identical(same)
         public string EmpConfirmPassword { get; set; }
+        [Required]
         [RegularExpression(@"^\d+(\.\d{1,3})?$", ErrorMessage = "Max 3 Digits Allowed !!")]
         [Range(0.999, 9999.999, ErrorMessage = "Please Enter Max Range 9999 !!")]
         public decimal? Decimal { get; set; }
+        [ReadOnly(true)]//We Can't Overwrite/Insert Data Here(Because Model Not Accepted), so maked field readonly also
+        public string EmpOrganisationName { get; set; }
     }
-}//Note:- Every DataAnotation its own Default Message but we can declare own Custom Message,
-//if auto required message showed other prop's without data anotation providing please provide null by ? that's prop
+}//Note:- Every DataAnotation its own Default Message but we can declare own Custom Message also using by ErrorMessage.
+ //if auto required message showed other prop's without data anotation appling please provide null by ? that's prop.
+ //Required DataAnnotations is must For Performing Other DataAnnotations
