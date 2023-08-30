11effab8174a4a48f8e16984561b2776941bdf11
Gulshan Kumar <gulshankumar.mailid01@gmail.com>
2023-08-30 21:22:45 +0530

    ViewBag and TempData Compleate

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MVC.Controllers
         public ActionResult Index()
         {
             ViewData["Message"] = "Message from View Data !!";
-            ViewData["CurrentTime"] = DateTime.Now.ToLongTimeString();
+            ViewData["CurrentTime"] = DateTime.Now.ToLongTimeString();//7:18:54 PM
 
             string[] Fruits = { "Apple", "Banana", "Grapes", "Orange" };
             ViewData["FruitsArray"] = Fruits;
@@ -24,12 +24,45 @@ namespace MVC.Controllers
                 "Volley Ball"
             };
 
-            Employee Gulshan = new Employee();//Model has no data so, I provided static
-            Gulshan.EmployeeId = 1;
-            Gulshan.EmployeeName = "Gulshan";
-            Gulshan.EmployeeEmail = "gulshankumar.mailid01@gmail.com";
+            Employee Gulshan = new Employee//Model has no data so, I provided static
+            {
+                EmployeeId = 1,
+                EmployeeName = "Gulshan",
+                EmployeeEmail = "gulshankumar.mailid01@gmail.com"
+            };
             ViewData["Employee"] = Gulshan;//object passing in ViewData
             //---------------------------------------------------------------------------------------------
+
+            ViewBag.Message = "Message from View Bag !!";
+            ViewBag.CurrentTime = DateTime.Now.ToLocalTime();//8/30/2023 7:19:25 PM
+            ViewBag.FruitsArray = Fruits;
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Hockey",
+                "Football",
+                "Volley Ball"
+            };
+            ViewBag.Employee = Gulshan;
+            //---------------------------------------------------------------------------------------------
+
+            TempData["Data"] = "Message from Temp Data !!";
+            string[] games = { "Cricket", "Hockey", "Football", "Volley Ball" };
+            TempData["GamesArrays"] = games;
+            return RedirectToAction(nameof(About));
+            //return View();
+        }
+        public ActionResult About()
+        {
+            if (TempData["Data"] != null)
+            {
+                TempData["Data"].ToString();
+            }
+            TempData.Keep();
+            return View();
+        }
+        public ActionResult Contact()
+        {
             return View();
         }
     }
