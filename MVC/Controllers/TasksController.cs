f4d0e1efd3114ddf4de9ee3d56d7e2cee6e678c7
Gulshan Kumar <gulshankumar.mailid01@gmail.com>
2024-06-03 21:39:40 +0530

    ExporttoExcell and RDLC Report Done

---
+++ MVC/Controllers/TasksController.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System;
+using System.Web.Mvc;
+using ClosedXML.Excel;
+
+namespace MVC.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExportToExcel(FormCollection formCollection)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            try
+            {
+                SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DataContext"].ConnectionString);
+                SqlCommand cmd = new SqlCommand("SP_SelectEmployee", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    dt.TableName = "Employee";
+                    using (XLWorkbook wb = new XLWorkbook())
+                    {
+                        wb.Worksheets.Add(dt);
+                        wb.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                        wb.Style.Font.Bold = true;
+                        Response.Clear();
+                        Response.Buffer = true;
+                        Response.Charset = "";
+                        Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheet.sheet";
+                        Response.AddHeader("content-disposition", "attachment;filename=EmployeeList.xlsx");
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            wb.SaveAs(ms);
+                            ms.WriteTo(Response.OutputStream);
+                            Response.Flush();
+                            Response.Close();
+                        }
+                    };
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC/PrintRDLC.aspx.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MVC
+{
+    public partial class PrintRDLC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LocalReport rep = rptReportViewer.LocalReport;
+                rep.ReportPath = "EmployeeList.rdlc";
+                ReportDataSource rds = new ReportDataSource();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter();
+                SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DataContext"].ConnectionString);
+                SqlCommand cmd = new SqlCommand("SP_SelectEmployee", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    rds.Name = "DS_PrintRDLC";
+                    rds.Value = dt;
+                    rep.DataSources.Add(rds);
+                }
+            }
+        }
+    }
+}
