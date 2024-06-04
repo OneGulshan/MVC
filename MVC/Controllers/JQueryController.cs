b0208e9b6ff7f47fa30a91e4b7ca27b1f0ebf5ce
Gulshan Kumar <gulshankumar.mailid01@gmail.com>
2024-06-04 14:13:19 +0530

    JQueryProject Deleted and Apply Jquery Crud in MVC Project

--- MVC/Controllers/JQueryController.cs
+++ MVC/Controllers/JQueryController.cs
@@ -1,16 +1,15 @@
-﻿using Newtonsoft.Json;
-using System.Configuration;
-using System.Data.SqlClient;
+﻿using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Web.Mvc;
-using jQueryMVC.Models;
-using System;
+using MVC.Models;
+using Newtonsoft.Json;
 
-namespace jQueryMVC.Controllers
+namespace MVC.Controllers
 {
-    public class EmployeeController : Controller
+    public class JQueryController : Controller
     {
-        private readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+        private readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
         public ActionResult Create()
         {
             return View();
@@ -18,32 +17,36 @@ namespace jQueryMVC.Controllers
 
         public void InsertUpdate(Emp obj)
         {
-            if (obj.empid > 0)
+            if (obj.Empid > 0)
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("sp_Employee", con);
-                cmd.CommandType = CommandType.StoredProcedure;
+                SqlCommand cmd = new SqlCommand("sp_Employee", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
                 cmd.Parameters.AddWithValue("@mode", 5);
-                cmd.Parameters.AddWithValue("@empid", obj.empid);
-                cmd.Parameters.AddWithValue("@name", obj.name);
-                cmd.Parameters.AddWithValue("@address", obj.address);
-                cmd.Parameters.AddWithValue("@age", obj.age);
-                cmd.Parameters.AddWithValue("@country", obj.country);
-                cmd.Parameters.AddWithValue("@state", obj.state);
+                cmd.Parameters.AddWithValue("@empid", obj.Empid);
+                cmd.Parameters.AddWithValue("@name", obj.Name);
+                cmd.Parameters.AddWithValue("@address", obj.Address);
+                cmd.Parameters.AddWithValue("@age", obj.Age);
+                cmd.Parameters.AddWithValue("@country", obj.Country);
+                cmd.Parameters.AddWithValue("@state", obj.State);
                 cmd.ExecuteNonQuery();
                 con.Close();
             }
             else
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("sp_Employee", con);
-                cmd.CommandType = CommandType.StoredProcedure;
+                SqlCommand cmd = new SqlCommand("sp_Employee", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
                 cmd.Parameters.AddWithValue("@mode", 1);
-                cmd.Parameters.AddWithValue("@name", obj.name);
-                cmd.Parameters.AddWithValue("@address", obj.address);
-                cmd.Parameters.AddWithValue("@age", obj.age);
-                cmd.Parameters.AddWithValue("@country", obj.country);
-                cmd.Parameters.AddWithValue("@state", obj.state);
+                cmd.Parameters.AddWithValue("@name", obj.Name);
+                cmd.Parameters.AddWithValue("@address", obj.Address);
+                cmd.Parameters.AddWithValue("@age", obj.Age);
+                cmd.Parameters.AddWithValue("@country", obj.Country);
+                cmd.Parameters.AddWithValue("@state", obj.State);
                 cmd.ExecuteNonQuery();
                 con.Close();
             }
@@ -51,8 +54,10 @@ namespace jQueryMVC.Controllers
 
         public JsonResult GetAllData()
         {
-            SqlCommand cmd = new SqlCommand("sp_Employee", con);
-            cmd.CommandType = CommandType.StoredProcedure;
+            SqlCommand cmd = new SqlCommand("sp_Employee", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cmd.Parameters.AddWithValue("@mode", 2);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -64,20 +69,24 @@ namespace jQueryMVC.Controllers
         public void DeleteData(Emp obj)
         {
             con.Open();
-            SqlCommand cmd = new SqlCommand("sp_Employee", con);
-            cmd.CommandType = CommandType.StoredProcedure;
+            SqlCommand cmd = new SqlCommand("sp_Employee", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cmd.Parameters.AddWithValue("@mode", 3);
-            cmd.Parameters.AddWithValue("@empid", obj.empid);
+            cmd.Parameters.AddWithValue("@empid", obj.Empid);
             cmd.ExecuteNonQuery();
             con.Close();
         }
 
         public JsonResult EditData(Emp obj)
         {
-            SqlCommand cmd = new SqlCommand("sp_Employee", con);
-            cmd.CommandType = CommandType.StoredProcedure;
+            SqlCommand cmd = new SqlCommand("sp_Employee", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cmd.Parameters.AddWithValue("@mode", 4);
-            cmd.Parameters.AddWithValue("@empid", obj.empid);
+            cmd.Parameters.AddWithValue("@empid", obj.Empid);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
@@ -87,8 +96,10 @@ namespace jQueryMVC.Controllers
 
         public JsonResult GetAllCountry()
         {
-            SqlCommand cmd = new SqlCommand("sp_Employee", con);
-            cmd.CommandType = CommandType.StoredProcedure;
+            SqlCommand cmd = new SqlCommand("sp_Employee", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cmd.Parameters.AddWithValue("@mode", 6);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -99,8 +110,10 @@ namespace jQueryMVC.Controllers
 
         public JsonResult GetStatebyCountry(int A)
         {
-            SqlCommand cmd = new SqlCommand("sp_Employee", con);
-            cmd.CommandType = CommandType.StoredProcedure;
+            SqlCommand cmd = new SqlCommand("sp_Employee", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
             cmd.Parameters.AddWithValue("@mode", 7);
             cmd.Parameters.AddWithValue("@cid", A);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
---
+++ MVC/Models/Emp.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC.Models
+{
+    public class Emp
+    {
+        public int Empid { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public int Country { get; set; }
+        public int State { get; set; }
+    }
+}
